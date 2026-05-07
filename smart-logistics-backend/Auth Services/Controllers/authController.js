54af51d4d237a7c7630737859fb6f3a4cb78f3e1
Nawin chander <nawinnapzy289@gmail.com>
2026-05-07 20:33:04 +0530

    add

---
+++ smart-logistics-backend/Auth Services/Controllers/authController.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      user,
+       });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
